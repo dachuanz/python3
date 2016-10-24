7a193ada6979bd6287826f54f3a5a75a7bef0cbd
张大川 <dachuanz@gmail.com>
2016-10-24 15:38:29 +0800

    ww
    ww

---
+++ perfect.py
@@ -0,0 +1,11 @@
+def isperfect(num):
+    slist=[]
+    count =num-1
+    while count>=1:
+        if num % count == 0:
+            slist.append(count)
+        count-=1
+    if sum(slist)==num:
+        return True
+    else:
+        return False
