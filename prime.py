4f372612cef1ceb53969fb5e0c58a765a14b9df9
张大川 <dachuanz@gmail.com>
2016-10-24 14:53:32 +0800

    11
    11

--- prime.py
+++ prime.py
@@ -1,8 +1,9 @@
-#测试一个整数，是否为质数
-num =947
-count =num-1
-while count>0:
-    if num%count==0:
-        print(num,count)
-        break
-    count-=1
+import math
+def isprime(num):
+    count = int(math.sqrt(num))
+    while count>1:
+        if num % count == 0:
+            return False
+        count -= 1
+    else:
+         return True
